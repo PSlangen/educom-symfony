dfc269b833f34b08c9dc712e4f4a5f54eb4b1d27
Paul Slangen <plg.slangen@gmail.com>
2023-03-07 15:58:09 +0100

    Services

--- src/Controller/OptredenController.php
+++ src/Controller/OptredenController.php
@@ -6,33 +6,47 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
+use App\Service\OptredenService;
+
 use App\Entity\Optreden;
 use App\Entity\Artiest;
 
-
+#[Route('/optreden')]
 class OptredenController extends AbstractController
 {
+    private $os; 
 
-    private $artiestRep;
-    private $optredenRep;
+    public function __construct(OptredenService $os) {
+        $this->os = $os;      
+    }
 
+    #[Route('/save', name: 'optreden_save')] 
+    public function saveOptreden() {
 
+        $optreden = [
+            "poppodium_id" => 1,
+            "hoofdprogramma_id" => 1, 
+            "voorprogramma_id" => 2,
+            "omschrijving" => "Een avondje blues uit het boekje...",
+            "datum" => "2022-07-14",
+            "prijs" => 3800,
+            "ticket_url" => "https://melkweg.nl/ticket/",
+            "afbeelding_url" => "https://melkweg.nl/optreden/plaatje.jpg"
+        ];
 
-    #[Route('/optreden', name: 'app_optreden')]
-    public function index(): Response
-    {
-        return $this->render('optreden/index.html.twig', [
-            'controller_name' => 'OptredenController',
-        ]);
+        $result = $this->os->saveOptreden($optreden);
+        dd($result);
+
     }
 
     
-    #[Route('/test/{id}', name: 'test')]
-    public function test($id){
+    #[Route('/delete/{id}', name: 'test')]
+    public function delete($id){
         $this->artiestRep = $this->getDoctrine()->getRepository(Artiest::class);
         $this->optredenRep = $this->getDoctrine()->getRepository(Optreden::class);
-        $artiest = $this->artiestRep->find($id);
-        $optreden = $this->optredenRep->deleteOptredensByArtiest($artiest);
+        $artiest = $this->artiestRepository->find($id);
+        $optreden = $this->optredenRepository->deleteOptredensByArtiest($artiest);
+
         $artiestDeleted = $this->artiestRep->deleteArtiest($artiest->getId());
 
         dd($optreden);
--- src/Repository/OptredenRepository.php
+++ src/Repository/OptredenRepository.php
@@ -3,61 +3,29 @@
 namespace App\Repository;
 
 use App\Entity\Optreden;
-use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
-use Doctrine\Persistence\ManagerRegistry;
 
 use App\Entity\Artiest;
 use App\Entity\Poppodium;
 
+use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
+use Doctrine\Persistence\ManagerRegistry;
+
 /**
- * @extends ServiceEntityRepository<Optreden>
- *
  * @method Optreden|null find($id, $lockMode = null, $lockVersion = null)
  * @method Optreden|null findOneBy(array $criteria, array $orderBy = null)
  * @method Optreden[]    findAll()
  * @method Optreden[]    findBy(array $criteria, array $orderBy = null, $limit = null, $offset = null)
  */
-
 class OptredenRepository extends ServiceEntityRepository
 {
+
     public function __construct(ManagerRegistry $registry)
     {
         parent::__construct($registry, Optreden::class);
-            $this->artiestRepository = $this->_em->getRepository(Artiest::class);
-            $this->poppodiumRepository = $this->_em->getRepository(Poppodium::class);
-    }
-
-    public function save(Optreden $entity, bool $flush = false): void
-    {
-        $this->getEntityManager()->persist($entity);
-
-        if ($flush) {
-            $this->getEntityManager()->flush();
-        }
-    }
-
-    public function remove(Optreden $entity, bool $flush = false): void
-    {
-        $this->getEntityManager()->remove($entity);
-
-        if ($flush) {
-            $this->getEntityManager()->flush();
-        }
     }
 
     public function getAllOptredens() {
-        $data = $this->findAll();
-        return($data); 
-    } 
-
-    private function fetchArtiest($id) {
-        $artiest = $this->artiestRepository->fetchArtiest($id);
-        return($artiest);
-    }
-
-    private function fetchPoppodium($id) {
-        $podium = $this->poppodiumRepository->fetchPoppodium($id);
-        return($podium);
+        return($this->findAll());        
     }
 
     public function saveOptreden($params) {
@@ -68,16 +36,11 @@ class OptredenRepository extends ServiceEntityRepository
             $optreden = new Optreden();
         }
         
-        $optreden->setPodium($this->fetchPoppodium($params["poppodium_id"]));
-        $optreden->setArtiest($this->fetchArtiest($params["hoofdprogramma_id"]));
-
-        if(isset($params["voorprogramma_id"])) {
-            $optreden->setVoorprogramma($this->fetchArtiest($params["voorprogramma_id"]));
-        }
-
+        $optreden->setPodium($params["poppodium"]);
+        $optreden->setArtiest($params["hoofdprogramma"]);
+        $optreden->setVoorprogramma($params["voorprogramma"]);
         $optreden->setOmschrijving($params["omschrijving"]);
-        $optreden->setDatum(new \DateTime($params["datum"]));
-
+        $optreden->setDatum($params["datum"]);
         $optreden->setPrijs($params["prijs"]);
         $optreden->setTicketUrl($params["ticket_url"]);
         $optreden->setAfbeeldingUrl($params["afbeelding_url"]);
@@ -88,6 +51,7 @@ class OptredenRepository extends ServiceEntityRepository
         return($optreden);
         
     }
+
 
     public function deleteOptreden($id) {
     
